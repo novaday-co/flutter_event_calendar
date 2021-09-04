a0fcefcce3debce9d0bc8ff6e86778df7367700b
Hosein1398 <Hosein1398@gmail.com>
2021-09-04 14:24:22 +0430

    -- get events of a day

--- lib/src/handlers/EventSelector.dart
+++ lib/src/handlers/EventSelector.dart
@@ -18,4 +18,23 @@ class EventSelector {
 
     return EventCalendar.selectedEvents;
   }
+
+  List<Event> getEventsForDayIndex(int index) {
+    EventCalendar.selectedEvents = [];
+    var i = 0;
+    for (var item in EventCalendar.events) {
+      var eventDateTimeParts = item.dateTime.split(' ');
+      var calendarDateTimeParts =
+          EventCalendar.dateTime.split(' ')[0].split('-');
+
+      if (eventDateTimeParts[0] ==
+          "${calendarDateTimeParts[0]}-${calendarDateTimeParts[1]}-$index") {
+        item.listIndex = i;
+        EventCalendar.selectedEvents.add(item);
+      }
+      i++;
+    }
+
+    return EventCalendar.selectedEvents;
+  }
 }

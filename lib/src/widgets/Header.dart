96187e8a88e757b3e49171f383892b208916cffc
Hosein1398 <Hosein1398@gmail.com>
2021-09-05 16:06:16 +0430

    -- bug: ui alignment bug fix

--- lib/src/widgets/Header.dart
+++ lib/src/widgets/Header.dart
@@ -20,73 +20,89 @@ class Header extends StatelessWidget {
         ),
         child: Row(
           crossAxisAlignment: CrossAxisAlignment.center,
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
           // Title , next and previous button
           children: [
-            InkWell(
-              onTap: () {
-                CalendarSelector().nextMonth();
-                onHeaderChanged.call();
-              },
-              child: Padding(
-                padding: EdgeInsets.only(left: 10),
-                child: Icon(
-                  Icons.arrow_back_ios,
-                  size: 18,
-                ),
-              ),
-            ),
-            Row(
-              textDirection:
-                  EventCalendar.calendarProvider.isRTL() ? TextDirection.rtl : TextDirection.ltr,
-              children: [
-                GestureDetector(
-                  onTap: () {
-                    showModalBottomSheet(
-                      backgroundColor: Colors.transparent,
-                      context: context,
-                      builder: (BuildContext context) {
-                        return SelectMonth(
-                          onHeaderChanged: onHeaderChanged,
-                        );
-                      },
-                    );
-                  },
-                  child: Container(
-                    child: Text(
-                      '${CalendarSelector().getPart(format: PartFormat.month, responseType: 'string')}',
-                      style: TextStyle(
-                        fontWeight: FontWeight.w500,
-                        fontSize: 20,
-                        fontFamily: EventCalendar.font,
+            Expanded(
+              flex: 2,
+              child: Row(
+                children: [
+                  InkWell(
+                    onTap: () {
+                      CalendarSelector().nextMonth();
+                      onHeaderChanged.call();
+                    },
+                    child: Padding(
+                      padding: EdgeInsets.only(left: 10),
+                      child: Icon(
+                        Icons.arrow_back_ios,
+                        size: 18,
                       ),
                     ),
                   ),
-                ),
-                Text(' , '),
-                GestureDetector(
-                  onTap: () {
-                    showModalBottomSheet(
-                      backgroundColor: Colors.transparent,
-                      context: context,
-                      builder: (BuildContext context) {
-                        return SelectYear(
-                          onHeaderChanged: onHeaderChanged,
+                  if (!isInTodayIndex()) buildRefreshView(),
+                ],
+              ),
+            ),
+            Expanded(
+              flex: 3,
+              child: Center(
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  mainAxisSize: MainAxisSize.max,
+                  textDirection: EventCalendar.calendarProvider.isRTL()
+                      ? TextDirection.rtl
+                      : TextDirection.ltr,
+                  children: [
+                    GestureDetector(
+                      onTap: () {
+                        showModalBottomSheet(
+                          backgroundColor: Colors.transparent,
+                          context: context,
+                          builder: (BuildContext context) {
+                            return SelectMonth(
+                              onHeaderChanged: onHeaderChanged,
+                            );
+                          },
                         );
                       },
-                    );
-                  },
-                  child: Text(
-                    '${CalendarSelector().getPart(format: PartFormat.year, responseType: 'int')}',
-                    style: TextStyle(
-                      fontWeight: FontWeight.w500,
-                      fontSize: 20,
-                      fontFamily: EventCalendar.font,
+                      child: Container(
+                        child: Text(
+                          '${CalendarSelector().getPart(format: PartFormat.month, responseType: 'string')}',
+                          style: TextStyle(
+                            fontWeight: FontWeight.w500,
+                            fontSize: 20,
+                            fontFamily: EventCalendar.font,
+                          ),
+                        ),
+                      ),
                     ),
-                  ),
+                    Text(' , '),
+                    GestureDetector(
+                      onTap: () {
+                        showModalBottomSheet(
+                          backgroundColor: Colors.transparent,
+                          context: context,
+                          builder: (BuildContext context) {
+                            return SelectYear(
+                              onHeaderChanged: onHeaderChanged,
+                            );
+                          },
+                        );
+                      },
+                      child: Text(
+                        '${CalendarSelector().getPart(format: PartFormat.year, responseType: 'int')}',
+                        style: TextStyle(
+                          fontWeight: FontWeight.w500,
+                          fontSize: 20,
+                          fontFamily: EventCalendar.font,
+                        ),
+                      ),
+                    ),
+                  ],
                 ),
-              ],
+              ),
             ),
+            // if (!isInTodayIndex()) buildRefreshView(),
             InkWell(
               onTap: () {
                 CalendarSelector().previousMonth();
@@ -101,6 +117,28 @@ class Header extends StatelessWidget {
               ),
             ),
           ],
+        ),
+      ),
+    );
+  }
+
+  isInTodayIndex() {
+    return EventCalendar.dateTime.split(' ')[0] ==
+        CalendarSelector().getCurrentDateTime();
+  }
+
+  buildRefreshView() {
+    return Material(
+      shape: CircleBorder(),
+      child: InkWell(
+        customBorder: CircleBorder(),
+        onTap: () {
+          EventCalendar.dateTime = EventCalendar.calendarProvider.getDateTime();
+          onHeaderChanged.call();
+        },
+        child: Padding(
+          padding: EdgeInsets.all(5),
+          child: Icon(Icons.restore),
         ),
       ),
     );

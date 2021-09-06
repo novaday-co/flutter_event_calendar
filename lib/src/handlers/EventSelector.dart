cd208cf7eed7366450be2f0207914ce1fd084586
Hosein1398 <Hosein1398@gmail.com>
2021-09-06 15:38:15 +0430

    -- support getEvents from month view type

--- lib/src/handlers/EventSelector.dart
+++ lib/src/handlers/EventSelector.dart
@@ -19,16 +19,16 @@ class EventSelector {
     return EventCalendar.selectedEvents;
   }
 
-  List<Event> getEventsForDayIndex(int index) {
+  List<Event> getEventsByDayMonthYear(int year, int month, int dayIndex) {
     EventCalendar.selectedEvents = [];
     var i = 0;
+    final monthString = month < 10 ? "0$month" : "$month";
+    final dayString = dayIndex < 10 ? "0$dayIndex" : "$dayIndex";
     for (var item in EventCalendar.events) {
       var eventDateTimeParts = item.dateTime.split(' ');
       var calendarDateTimeParts =
           EventCalendar.dateTime.split(' ')[0].split('-');
-
-      if (eventDateTimeParts[0] ==
-          "${calendarDateTimeParts[0]}-${calendarDateTimeParts[1]}-$index") {
+      if (eventDateTimeParts[0] == "$year-$monthString-$dayString") {
         item.listIndex = i;
         EventCalendar.selectedEvents.add(item);
       }

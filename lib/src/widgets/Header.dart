5d7e3b4c46c88485cf3010f9eabedbc1ea3fc4fe
Hosein1398 <Hosein1398@gmail.com>
2021-09-06 15:37:33 +0430

    -- support directionality -- fix header in center

--- lib/src/widgets/Header.dart
+++ lib/src/widgets/Header.dart
@@ -13,84 +13,54 @@ class Header extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Padding(
-        padding: EdgeInsets.only(
-          top: 15,
-        ),
+    return Padding(
+      padding: EdgeInsets.only(top: 15, bottom: 15),
+      child: Directionality(
+        textDirection: EventCalendar.calendarProvider.isRTL()
+            ? TextDirection.rtl
+            : TextDirection.ltr,
         child: Row(
           crossAxisAlignment: CrossAxisAlignment.center,
           // Title , next and previous button
           children: [
-            Expanded(
-              flex: 2,
-              child: Row(
-                children: [
-                  InkWell(
-                    onTap: () {
-                      CalendarSelector().nextMonth();
-                      onHeaderChanged.call();
-                    },
-                    child: Padding(
-                      padding: EdgeInsets.only(left: 10),
-                      child: Icon(
-                        Icons.arrow_back_ios,
-                        size: 18,
-                      ),
+            Row(
+              children: [
+                InkWell(
+                  onTap: () {
+                    CalendarSelector().nextMonth();
+                    onHeaderChanged.call();
+                  },
+                  child: Padding(
+                    padding: EdgeInsets.symmetric(horizontal: 10),
+                    child: Icon(
+                      Icons.arrow_back_ios,
+                      size: 18,
                     ),
                   ),
-                  if (!isInTodayIndex()) buildRefreshView(),
-                ],
-              ),
+                ),
+                buildRefreshView(),
+              ],
             ),
             Expanded(
-              flex: 3,
-              child: Center(
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.end,
-                  mainAxisSize: MainAxisSize.max,
-                  textDirection: EventCalendar.calendarProvider.isRTL()
-                      ? TextDirection.rtl
-                      : TextDirection.ltr,
-                  children: [
-                    GestureDetector(
-                      onTap: () {
-                        showModalBottomSheet(
-                          backgroundColor: Colors.transparent,
-                          context: context,
-                          builder: (BuildContext context) {
-                            return SelectMonth(
-                              onHeaderChanged: onHeaderChanged,
-                            );
-                          },
-                        );
-                      },
-                      child: Container(
-                        child: Text(
-                          '${CalendarSelector().getPart(format: PartFormat.month, responseType: 'string')}',
-                          style: TextStyle(
-                            fontWeight: FontWeight.w500,
-                            fontSize: 20,
-                            fontFamily: EventCalendar.font,
-                          ),
-                        ),
-                      ),
-                    ),
-                    Text(' , '),
-                    GestureDetector(
-                      onTap: () {
-                        showModalBottomSheet(
-                          backgroundColor: Colors.transparent,
-                          context: context,
-                          builder: (BuildContext context) {
-                            return SelectYear(
-                              onHeaderChanged: onHeaderChanged,
-                            );
-                          },
-                        );
-                      },
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                mainAxisSize: MainAxisSize.max,
+                children: [
+                  GestureDetector(
+                    onTap: () {
+                      showModalBottomSheet(
+                        backgroundColor: Colors.transparent,
+                        context: context,
+                        builder: (BuildContext context) {
+                          return SelectMonth(
+                            onHeaderChanged: onHeaderChanged,
+                          );
+                        },
+                      );
+                    },
+                    child: Container(
                       child: Text(
-                        '${CalendarSelector().getPart(format: PartFormat.year, responseType: 'int')}',
+                        '${CalendarSelector().getPart(format: PartFormat.month, responseType: 'string')}',
                         style: TextStyle(
                           fontWeight: FontWeight.w500,
                           fontSize: 20,
@@ -98,23 +68,50 @@ class Header extends StatelessWidget {
                         ),
                       ),
                     ),
-                  ],
-                ),
+                  ),
+                  Text(' , '),
+                  GestureDetector(
+                    onTap: () {
+                      showModalBottomSheet(
+                        backgroundColor: Colors.transparent,
+                        context: context,
+                        builder: (BuildContext context) {
+                          return SelectYear(
+                            onHeaderChanged: onHeaderChanged,
+                          );
+                        },
+                      );
+                    },
+                    child: Text(
+                      '${CalendarSelector().getPart(format: PartFormat.year, responseType: 'int')}',
+                      style: TextStyle(
+                        fontWeight: FontWeight.w500,
+                        fontSize: 20,
+                        fontFamily: EventCalendar.font,
+                      ),
+                    ),
+                  ),
+                ],
               ),
             ),
             // if (!isInTodayIndex()) buildRefreshView(),
-            InkWell(
-              onTap: () {
-                CalendarSelector().previousMonth();
-                onHeaderChanged.call();
-              },
-              child: Padding(
-                padding: EdgeInsets.only(right: 10),
-                child: Icon(
-                  Icons.arrow_forward_ios,
-                  size: 18,
+            Row(
+              children: [
+                SizedBox(width: 18),
+                InkWell(
+                  onTap: () {
+                    CalendarSelector().previousMonth();
+                    onHeaderChanged.call();
+                  },
+                  child: Padding(
+                    padding: EdgeInsets.only(right: 10),
+                    child: Icon(
+                      Icons.arrow_forward_ios,
+                      size: 18,
+                    ),
+                  ),
                 ),
-              ),
+              ],
             ),
           ],
         ),
@@ -123,22 +120,29 @@ class Header extends StatelessWidget {
   }
 
   isInTodayIndex() {
+    print(
+        "${EventCalendar.dateTime.split(' ')[0]} == ${CalendarSelector().getCurrentDateTime().split(' ')[0]}");
     return EventCalendar.dateTime.split(' ')[0] ==
-        CalendarSelector().getCurrentDateTime();
+        CalendarSelector().getCurrentDateTime().split(' ')[0];
   }
 
   buildRefreshView() {
-    return Material(
-      shape: CircleBorder(),
-      child: InkWell(
-        customBorder: CircleBorder(),
-        onTap: () {
-          EventCalendar.dateTime = EventCalendar.calendarProvider.getDateTime();
-          onHeaderChanged.call();
-        },
-        child: Padding(
-          padding: EdgeInsets.all(5),
-          child: Icon(Icons.restore),
+    return AnimatedOpacity(
+      duration: Duration(milliseconds: 300),
+      opacity: !isInTodayIndex() ? 1 : 0,
+      child: Material(
+        shape: CircleBorder(),
+        child: InkWell(
+          customBorder: CircleBorder(),
+          onTap: () {
+            EventCalendar.dateTime =
+                EventCalendar.calendarProvider.getDateTime();
+            onHeaderChanged.call();
+          },
+          child: Padding(
+            padding: EdgeInsets.symmetric(horizontal: 5),
+            child: Icon(Icons.restore),
+          ),
         ),
       ),
     );

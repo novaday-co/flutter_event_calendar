ac1d0f4d48d4d2aaf0307338805b05e8dbf21b3d
Hosein1398 <Hosein1398@gmail.com>
2021-09-06 15:47:26 +0430

    -- Month view calendar type

--- lib/src/widgets/CalendarMonth.dart
+++ lib/src/widgets/CalendarMonth.dart
@@ -1,11 +1,23 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 import 'package:flutter_event_calendar/flutter_event_calendar.dart';
 import 'package:flutter_event_calendar/src/handlers/CalendarSelector.dart';
+import 'package:flutter_event_calendar/src/handlers/EventCalendar.dart';
 import 'package:flutter_event_calendar/src/handlers/EventSelector.dart';
+import 'package:flutter_event_calendar/src/widgets/Day.dart';
 
-class CalendarMonth extends StatelessWidget {
-  CalendarMonth({Key? key}) : super(key: key);
-  final dayNames = CalendarSelector().getDayNames();
+class CalendarMonth extends StatefulWidget {
+  Function onCalendarChanged;
+
+  CalendarMonth({required this.onCalendarChanged, Key? key}) : super();
+
+  @override
+  State<CalendarMonth> createState() => _CalendarMonthState();
+}
+
+class _CalendarMonthState extends State<CalendarMonth> {
+  List<String> dayNames = CalendarSelector().getDayNames();
 
   @override
   Widget build(BuildContext context) {
@@ -17,13 +29,20 @@ class CalendarMonth extends StatelessWidget {
 
   _buildDayName() {
     return Row(
+      textDirection: EventCalendar.calendarProvider.isRTL()
+          ? TextDirection.rtl
+          : TextDirection.ltr,
       children: List.generate(
         7,
         (index) => Expanded(
           child: Center(
+            heightFactor: 2,
             child: Text(
               dayNames[index],
-              style: TextStyle(fontFamily: EventCalendar.font),
+              style: TextStyle(
+                  fontSize: 15,
+                  fontWeight: FontWeight.bold,
+                  fontFamily: EventCalendar.font),
             ),
           ),
         ),
@@ -32,31 +51,135 @@ class CalendarMonth extends StatelessWidget {
   }
 
   _buildMonthView() {
-    final monthDays = CalendarSelector().getDays();
-    final firstDayIndex = getFirstDayOfMonth();
-    final lastDayIndex = firstDayIndex + getLastDayOfMonth();
-    return SizedBox(
-      height: 200,
-      child: GridView.builder(
-        itemCount: 35,
+    final currentMonth = CalendarSelector()
+        .getPart(format: PartFormat.month, responseType: 'int');
 
-        gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-            crossAxisCount: 7, childAspectRatio: 1.5),
-        itemBuilder: (context, index) {
-          if(index >= firstDayIndex && index< lastDayIndex )
-            return Center(child: Text("${index - firstDayIndex + 1}"),);
-          return Center(child: Text("NaN"));
-        },
+    // final monthDays = CalendarSelector().getDays(currentMonth);
+    final int firstDayIndex = getFirstDayOfMonth();
+    final int lastDayIndex = firstDayIndex + getLastDayOfMonth();
+    final lastMonthLastDay = getLastMonthLastDay();
+    final int cDayIndex =
+        CalendarSelector().getPart(format: PartFormat.day, responseType: 'int');
+
+    return SizedBox(
+      height: 260,
+      child: Directionality(
+        textDirection: EventCalendar.calendarProvider.isRTL()
+            ? TextDirection.rtl
+            : TextDirection.ltr,
+        child: GridView.builder(
+            physics: NeverScrollableScrollPhysics(),
+            itemCount: 42,
+            gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+              crossAxisCount: 7,
+              childAspectRatio: 1.4,
+            ),
+            itemBuilder: (context, index) => _buildItem(index, firstDayIndex,
+                lastDayIndex, lastMonthLastDay, currentMonth, cDayIndex)),
       ),
     );
   }
-  getFirstDayOfMonth(){
-    final monthDays = CalendarSelector().getDays();
-    print("index of ${dayNames.indexOf(monthDays[1])}");
+
+  _buildItem(int index, int firstDayIndex, int lastDayIndex,
+      int lastMonthLastDay, int currentMonth, int cDayIndex) {
+    int dayIndex = -1;
+
+    final isCurrentMonthDays = index >= firstDayIndex && index < lastDayIndex;
+    final isNextMonthDays = index >= lastDayIndex;
+
+    if (isCurrentMonthDays)
+      dayIndex = index - firstDayIndex + 1;
+    else if (isNextMonthDays)
+      dayIndex = index - lastDayIndex + 1;
+    else
+      dayIndex = lastMonthLastDay - (firstDayIndex - index) + 1;
+
+    if (isCurrentMonthDays)
+      return Center(
+        child: Day(
+          month: getMonth(currentMonth),
+          year: getYear(currentMonth),
+          dayIndex: dayIndex,
+          weekDay: '',
+          selected: dayIndex == cDayIndex,
+          onCalendarChanged: () {
+            CalendarSelector().goToDay(dayIndex);
+            widget.onCalendarChanged.call();
+          },
+          mini: true,
+        ),
+      );
+    else if (isNextMonthDays)
+      return Center(
+          child: Day(
+              dayIndex: dayIndex,
+              useUnselectedEffect: true,
+              weekDay: '',
+              month: getMonth(currentMonth + 1),
+              year: getYear(currentMonth + 1),
+              onCalendarChanged: () {
+                CalendarSelector().nextMonth();
+                CalendarSelector().goToDay(dayIndex);
+                widget.onCalendarChanged.call();
+              },
+              selected: false,
+              mini: true));
+    else if (dayIndex > 0) {
+      return Center(
+          child: Day(
+              dayIndex: dayIndex,
+              useUnselectedEffect: true,
+              month: getMonth(currentMonth - 1),
+              year: getYear(currentMonth - 1),
+              weekDay: '',
+              onCalendarChanged: () {
+                CalendarSelector().previousMonth();
+                CalendarSelector().goToDay(dayIndex);
+                widget.onCalendarChanged.call();
+              },
+              selected: false,
+              mini: true));
+    }
+    return SizedBox();
+  }
+
+  int getFirstDayOfMonth() {
+    final currentMonth = CalendarSelector()
+        .getPart(format: PartFormat.month, responseType: 'int');
+    final monthDays = CalendarSelector().getDays(currentMonth);
+    print("${monthDays[1]}");
+    print("${dayNames}");
     return dayNames.indexOf(monthDays[1]);
   }
 
-  getLastDayOfMonth() {
-    return CalendarSelector().getDays().keys.last;
+  int getLastDayOfMonth() {
+    final currentMonth = CalendarSelector()
+        .getPart(format: PartFormat.month, responseType: 'int');
+    return CalendarSelector().getDays(currentMonth).keys.last;
+  }
+
+  int getLastMonthLastDay() {
+    final cMonth = CalendarSelector()
+        .getPart(format: PartFormat.month, responseType: 'int');
+    if (cMonth - 1 < 1) {
+      return -1;
+    }
+    return CalendarSelector().getDays(cMonth - 1).keys.last;
+  }
+
+  getMonth(int month) {
+    if (month > 12)
+      return 1;
+    else if (month < 1) return 1;
+    return month;
+  }
+
+  getYear(int month) {
+    final year = CalendarSelector()
+        .getPart(format: PartFormat.year, responseType: 'int');
+    if (month > 12)
+      return year + 1;
+    else if (month < 1) return year - 1;
+    return year;
   }
 }

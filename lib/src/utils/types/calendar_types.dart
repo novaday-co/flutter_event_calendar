d87a90b9e07bf08e7f925d1d2d3b4a35c8b42e1c
Hosein1398 <Hosein1398@gmail.com>
2021-09-07 09:48:24 +0430

    -- add calendar view types

--- lib/src/utils/types/calendar_types.dart
+++ lib/src/utils/types/calendar_types.dart
@@ -1,4 +1,5 @@
 enum CalendarType { Jalali, Gregorian }
+enum CalendarViewType { Daily, Monthly }
 enum HeaderMonthStringTypes { Full, Short }
 enum HeaderWeekDayStringTypes { Full, Short }
 enum PartFormat { month, day, year }

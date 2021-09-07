4ebc64ac09f0ff943425ae2fd2dd1866d83d358b
Hosein1398 <Hosein1398@gmail.com>
2021-09-07 09:59:59 +0430

    -- better unProvided instance msg

--- lib/src/providers/instance_provider.dart
+++ lib/src/providers/instance_provider.dart
@@ -1,15 +1,15 @@
-import 'package:flutter_event_calendar/src/providers/calendares/base_calendar_provider.dart';
+import 'package:flutter_event_calendar/src/providers/calendares/calendar_provider.dart';
 import 'package:flutter_event_calendar/src/providers/calendares/en_calendar.dart';
 import 'package:flutter_event_calendar/src/providers/calendares/fa_calendar.dart';
 
-BaseCalendarProvider createInstance(String locale) {
-  final Map<String, BaseCalendarProvider> _factories = {
+CalendarProvider createInstance(String locale) {
+  final Map<String, CalendarProvider> _factories = {
     'fa': FaCalendar(),
     'en': EnCalendar()
   };
   if (!_factories.keys.contains(locale)) {
     throw Exception(
-        "Cannot create instance of calendar, check locale name or create a class that provides BaseCalendarProvider methods.");
+        "Cannot create instance of calendar, check locale name or create a class that implements BaseCalendarProvider");
   }
 
   return _factories[locale]!;

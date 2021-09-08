4ddec93fc6989a88a101ad11a5f75d3dee23b42c
Hosein1398 <Hosein1398@gmail.com>
2021-09-08 14:36:53 +0430

    -- applying locale translation

---
+++ lib/src/dictionaries/en.dart
@@ -0,0 +1,99 @@
+class En{
+  static Map fullMonthNames = const {
+    'fa': [
+      'ژانویه',
+      'فوریه',
+      'مارس',
+      'آوریل',
+      'مه',
+      'ژوئن',
+      'ژوئیه',
+      'اوت',
+      'سپتامبر',
+      'اکتبر',
+      'نوامبر',
+      'دسامبر'
+    ],
+    'en' :[
+      'January',
+      'February',
+      'March',
+      'April',
+      'May',
+      'June',
+      'July',
+      'August',
+      'September',
+      'October',
+      'November',
+      'December'
+    ]
+  };
+  static Map shortMonthNames = const {
+    'fa': [
+      'ژان',
+      'قور',
+      'مار',
+      'آور',
+      'مه',
+      'ژوئ',
+      'ژوئی',
+      'اوت',
+      'سپت',
+      'اکت',
+      'نوا',
+      'دسا'
+    ],
+    'en': [
+      'Jan',
+      'Feb',
+      'Mar',
+      'Apr',
+      'May',
+      'Jun',
+      'Jul',
+      'Aug',
+      'Sep',
+      'Oct',
+      'Nov',
+      'Dec'
+    ]
+  };
+
+  static Map fullDayNames = const {
+    'fa': [
+      'یکشنبه',
+      'دوشنبه',
+      'سه شنبه',
+      'چهارشنبه',
+      'پنجشنبه',
+      'جمعه',
+      'شنبه'
+    ],
+    'en': [
+      'Sunday',
+      'Monday',
+      'Tuesday',
+      'Wednesday',
+      'Thursday',
+      'Friday',
+      'Saturday'
+    ]
+  };
+  static Map shortDayNames = const {
+    'fa': ['سان','مان','تیو','وند','ترز','فرا','سات'],
+    'en': ['Su', 'Mo', 'Tu', 'We', 'Th', 'Fr', 'Sa']
+  };
+  static Map titles = const {
+    'fa': {
+      'empty': 'خالی',
+      'month_selector': 'یک ماه را انتخاب کنید',
+      'year_selector': 'یک سال را انتخاب کنید',
+    },
+    'en': {
+      'empty': 'Empty',
+      'month_selector': 'Choose a month',
+      'year_selector': 'Choose a year',
+    }
+  };
+}

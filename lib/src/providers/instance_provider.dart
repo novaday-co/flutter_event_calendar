cdf3bda3c3f150a73f0313b3391a417706fcff57
Hosein1398 <Hosein1398@gmail.com>
2021-09-08 16:08:12 +0430

    refactor: better instance creation

--- lib/src/providers/instance_provider.dart
+++ lib/src/providers/instance_provider.dart
@@ -1,16 +1,17 @@
+import 'package:flutter_event_calendar/flutter_event_calendar.dart';
 import 'package:flutter_event_calendar/src/providers/calendares/calendar_provider.dart';
 import 'package:flutter_event_calendar/src/providers/calendares/en_calendar.dart';
 import 'package:flutter_event_calendar/src/providers/calendares/fa_calendar.dart';
 
-CalendarProvider createInstance(String locale) {
-  final Map<String, CalendarProvider> _factories = {
-    'fa': FaCalendar(),
-    'en': EnCalendar()
+CalendarProvider createInstance(CalendarType cType) {
+  final Map<CalendarType, CalendarProvider> _factories = {
+    CalendarType.Jalali: FaCalendar(),
+    CalendarType.Gregorian: EnCalendar()
   };
-  if (!_factories.keys.contains(locale)) {
+  if (!_factories.keys.contains(cType)) {
     throw Exception(
-        "Cannot create instance of calendar, check locale name or create a class that implements BaseCalendarProvider");
+        "Cannot create instance of calendar, check available calendar types or create your own calendar that implements BaseCalendarProvider");
   }
 
-  return _factories[locale]!;
+  return _factories[cType]!;
 }

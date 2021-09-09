831236945e0a60fb951305788ccf0b21af39d500
farid froozan <froozan@yahoo.com>
2021-09-09 17:21:51 +0430

    fix a bug

--- lib/src/providers/calendares/calendar_provider.dart
+++ lib/src/providers/calendares/calendar_provider.dart
@@ -4,36 +4,19 @@ import 'package:flutter_event_calendar/src/utils/calendar_types.dart';
 abstract class CalendarProvider {
 
   bool isRTL();
-
-  /// month direction configs
-  String getNextMonthDateTime();
-
-  String getPreviousMonthDateTime();
-
-  /// day direction config
-  String getNextDayDateTime();
-
-  String getPreviousDayDateTime();
-
-  String getDateTime();
-
-  /// Month configs
-  List<int> getYears();
-
   Map getMonthDays(int index);
-
   Map getMonthDaysShort(int index);
-
+  String getNextMonthDateTime();
+  String getPreviousMonthDateTime();
+  String getNextDayDateTime();
+  String getPreviousDayDateTime();
+  String getDateTime();
   String getMonthName(index);
-
-  /// goto methods
   String goToMonth(index);
-
   String goToDay(index);
-
   String goToYear(int index);
-
-  /// part methods
   int getDateTimePart(PartFormat format);
+  List<int> getYears();
+  List<String> getNameOfDays() ;
 
 }

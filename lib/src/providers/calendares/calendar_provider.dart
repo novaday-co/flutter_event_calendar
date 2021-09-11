711b8347fa828e4469ccb36890a9690d46737557
Hosein1398 <Hosein1398@gmail.com>
2021-09-11 16:05:57 +0430

    refactor: change base time format

--- lib/src/providers/calendares/calendar_provider.dart
+++ lib/src/providers/calendares/calendar_provider.dart
@@ -1,4 +1,4 @@
-import 'package:flutter/material.dart';
+import 'package:flutter_event_calendar/flutter_event_calendar.dart';
 import 'package:flutter_event_calendar/src/utils/calendar_types.dart';
 
 abstract class CalendarProvider {
@@ -6,15 +6,15 @@ abstract class CalendarProvider {
   bool isRTL();
   Map getMonthDays(int index);
   Map getMonthDaysShort(int index);
-  String getNextMonthDateTime();
-  String getPreviousMonthDateTime();
-  String getNextDayDateTime();
-  String getPreviousDayDateTime();
-  String getDateTime();
+  EDateTime getNextMonthDateTime();
+  EDateTime getPreviousMonthDateTime();
+  EDateTime getNextDayDateTime();
+  EDateTime getPreviousDayDateTime();
+  EDateTime getDateTime();
   String getMonthName(index);
-  String goToMonth(index);
-  String goToDay(index);
-  String goToYear(int index);
+  EDateTime goToMonth(index);
+  EDateTime goToDay(index);
+  EDateTime goToYear(int index);
   int getDateTimePart(PartFormat format);
   List<int> getYears();
   List<String> getNameOfDays() ;

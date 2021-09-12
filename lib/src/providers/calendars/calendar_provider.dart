1c2d93239bedae13c0378652245f3d0fe3400b08
Hosein1398 <Hosein1398@gmail.com>
2021-09-12 11:28:54 +0430

    refactor: rename EDateTime style: clean up

--- lib/src/providers/calendars/calendar_provider.dart
+++ lib/src/providers/calendars/calendar_provider.dart
@@ -2,21 +2,33 @@ import 'package:flutter_event_calendar/flutter_event_calendar.dart';
 import 'package:flutter_event_calendar/src/utils/calendar_types.dart';
 
 abstract class CalendarProvider {
-
   bool isRTL();
-  Map getMonthDays(int index);
-  Map getMonthDaysShort(int index);
-  EDateTime getNextMonthDateTime();
-  EDateTime getPreviousMonthDateTime();
-  EDateTime getNextDayDateTime();
-  EDateTime getPreviousDayDateTime();
-  EDateTime getDateTime();
-  String getMonthName(index);
-  EDateTime goToMonth(index);
-  EDateTime goToDay(index);
-  EDateTime goToYear(int index);
-  int getDateTimePart(PartFormat format);
-  List<int> getYears();
-  List<String> getNameOfDays() ;
 
+  Map getMonthDays(int index);
+
+  Map getMonthDaysShort(int index);
+
+  EventDateTime getNextMonthDateTime();
+
+  EventDateTime getPreviousMonthDateTime();
+
+  EventDateTime getNextDayDateTime();
+
+  EventDateTime getPreviousDayDateTime();
+
+  EventDateTime getDateTime();
+
+  String getMonthName(index);
+
+  EventDateTime goToMonth(index);
+
+  EventDateTime goToDay(index);
+
+  EventDateTime goToYear(int index);
+
+  int getDateTimePart(PartFormat format);
+
+  List<int> getYears();
+
+  List<String> getNameOfDays();
 }

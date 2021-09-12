4acb6aaf9a5ac7fda56e70647f8718ff43ea29d0
Hosein1398 <Hosein1398@gmail.com>
2021-09-12 11:35:02 +0430

    style: clean code refactor: make translator static

--- lib/src/handlers/translator.dart
+++ lib/src/handlers/translator.dart
@@ -1,9 +1,9 @@
 import 'package:flutter_event_calendar/src/handlers/event_calendar.dart';
 import 'package:flutter_event_calendar/src/utils/calendar_types.dart';
 import '../dictionaries/dictionary.dart';
-class Translator {
 
-  String getPartTranslate(format, index) {
+class Translator {
+  static String getPartTranslate(format, index) {
     switch (format) {
       case PartFormat.month:
         return EventCalendar.calendarProvider.getMonthName(index);
@@ -12,21 +12,22 @@ class Translator {
     }
   }
 
-  String getTranslation(String word) =>
+  static String getTranslation(String word) =>
       titles[EventCalendar.calendarLanguage][word];
 
-  List<String> getShortNameOfDays() =>
+  static List<String> getShortNameOfDays() =>
       shortDayNames[EventCalendar.calendarLanguage][EventCalendar.calendarType];
 
-  List<String> getFullNameOfDays() =>
+  static List<String> getFullNameOfDays() =>
       fullDayNames[EventCalendar.calendarLanguage][EventCalendar.calendarType];
 
-  List<String> getFullMonthNames() =>
-      fullMonthNames[EventCalendar.calendarLanguage][EventCalendar.calendarType];
+  static List<String> getFullMonthNames() =>
+      fullMonthNames[EventCalendar.calendarLanguage]
+          [EventCalendar.calendarType];
 
-  List<String> getShortMonthNames() =>
-      shortMonthNames[EventCalendar.calendarLanguage][EventCalendar.calendarType];
+  static List<String> getShortMonthNames() =>
+      shortMonthNames[EventCalendar.calendarLanguage]
+          [EventCalendar.calendarType];
 
-  bool isRTL() => 
-      directionIsRTL[EventCalendar.calendarLanguage];
+  static bool isRTL() => directionIsRTL[EventCalendar.calendarLanguage];
 }

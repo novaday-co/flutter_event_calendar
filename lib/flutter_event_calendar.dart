f88934b7a3310cd4a56659208e08008fbbedb5ba
Hosein1398 <Hosein1398@gmail.com>
2021-09-15 15:04:49 +0430

    -- export models

--- lib/flutter_event_calendar.dart
+++ lib/flutter_event_calendar.dart
@@ -3,3 +3,5 @@ library flutter_event_calendar;
 export 'src/handlers/event_calendar.dart';
 export 'src/utils/calendar_types.dart';
 export 'src/models/datetime.dart';
+export 'src/models/style/day_style.dart';
+export 'src/models/event.dart';

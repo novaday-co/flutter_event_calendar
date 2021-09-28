36cd0f71c38b53bb6f0991116b70a56eb273d78f
novadaycompany <hosein1398@gmail.com>
2021-09-28 10:33:41 -0700

    fix: update example code

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -4,7 +4,6 @@ import 'package:flutter/material.dart';
 import 'package:flutter_event_calendar/flutter_event_calendar.dart';
 
 void main() {
-
   runApp(MyApp());
 }
 
@@ -26,32 +25,39 @@ class MyApp extends StatelessWidget {
 class MyHomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    var text = "text";
     // than having to individually change instances of widgets.
     return Scaffold(
       body: EventCalendar(
-        headerMonthStringType: HeaderMonthStringTypes.Full,
-        headerWeekDayStringType: HeaderWeekDayStringTypes.Short,
-        dayEventCountViewType: DayEventCountViewType.LABEL,
-        dayEventCountTextColor: Colors.white,
-        dayEventCountColor: Colors.blue,
         calendarType: CalendarType.Jalali,
         calendarLanguage: 'fa',
-        viewType: CalendarViewType.Monthly,
         canSelectViewType: true,
+        headersStyle: HeadersStyle(monthStringType: MonthStringTypes.Full),
+        onChangeDateTime: (dateTime) {
+          print("$dateTime");
+        },
+        middleWidget: (dateTime){
+          return Container(
+            color: Colors.blueGrey,
+            height: 75,
+            child: Center(child: Text("Program what you think ${dateTime.toString()}",style: TextStyle(color: Colors.white,fontSize: 18),),),
+          );
+        },
         events: [
           Event(
             title: 'Laravel Event',
             description:
                 'The largest Laravel event of the year, streamed directly to you. We’ve put together a full day of talks featuring some of Laravel’s brightest minds, and streaming them directly to your home or office.',
-            dateTime: '1400-06-31 20:00',
+            dateTime: EventDateTime(year: 1400, month: 07, day: 22),
           ),
+
           Event(
             title: 'Vue.js Event',
             description:
                 'Friday 18th September exclusive news from Evan You Live on our Youtube channel.' +
                     'This is FREE to the whole community & part of the #vuejsglobal conference experience.' +
                     'Subscribe Today to set up your notification.',
-            dateTime: '2021-08-31 20:00',
+            dateTime: EventDateTime(year: 1400, month: 07, day: 24),
           ),
         ],
       ),

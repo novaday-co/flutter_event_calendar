8e7edae801645bc7f04ed6e70bf598964ef28d58
parisa <parisa.baastani@gmail.com>
2022-07-16 10:09:35 +0430

    chore: delete redundant files.

--- lib/src/models/style/select_month_style.dart
+++
@@ -1,8 +0,0 @@
-import 'package:flutter/material.dart';
-
-class MonthStyle{
-  String font;
-  Color? selectedColor;
-
-  MonthStyle({this.font = '', this.selectedColor});
-}
--- lib/src/models/style/select_year_style.dart
+++
@@ -1,8 +0,0 @@
-import 'package:flutter/material.dart';
-
-class YearStyle{
-  String font;
-  Color? selectedColor;
-
-  YearStyle({this.font = '', this.selectedColor});
-}

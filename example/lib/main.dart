8358f2c2995bc9c5b7622bcdba7e696dcd481e55
Parisima <parisimaetemadi9675@gmail.com>
2022-09-10 09:33:51 +0430

    refactor

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -8,7 +8,13 @@ import 'injection.dart';
 
 void main() {
   setUp();
-
+  final list1 = <int>[1, 2, 3, 4];
+  final list2 = <int>[5, 6, 7, 8, 9];
+// Copies the 4th and 5th items in list2 as the 2nd and 3rd items
+// of list1.
+  const skipCount = 3;
+  list1.setRange(1, 3, list2, 3);
+  print(list1.getRange(0, list1.length)); // [1, 8, 9, 4]
   runApp(MyApp());
 }
 

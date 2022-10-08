6176c4f4ed1bf37a2688040866c6763b06205df7
Parisima <parisimaetemadi9675@gmail.com>
2022-10-08 13:15:27 +0330

    Chore:add calculate selected date list in Jalali and GREGORIAN to datetime.dart

--- lib/src/models/datetime.dart
+++ lib/src/models/datetime.dart
@@ -1,10 +1,15 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_event_calendar/flutter_event_calendar.dart';
+import 'package:flutter_event_calendar/src/handlers/calendar_utils.dart';
+import 'package:shamsi_date/extensions.dart';
+import 'package:shamsi_date/shamsi_date.dart';
+
 
 class CalendarDateTime {
   int year;
   int month;
   int day;
+  int? daysInMonth;
   int? toMonth;
   int? toDay;
   int? hour;
@@ -16,33 +21,34 @@ class CalendarDateTime {
 
   CalendarDateTime(
       {required this.year,
-      required this.month,
-      required this.day,
-      required this.calendarType,
-      this.isEnableDay = true,
-      this.toMonth,
-      this.toDay,
-      this.hour,
-      this.minute,
-      this.second,
-      this.color});
+        required this.month,
+        required this.day,
+        required this.calendarType,
+        this.isEnableDay = true,
+        this.toMonth,
+        this.toDay,
+        this.hour,
+        this.minute,
+        this.second,
+        this.color,
+        this.daysInMonth});
 
   //supported format 1400-9-12 20:00(:50)
-  static CalendarDateTime?  parseDateTime(String dateTime,CalendarType calendarType) {
+  static CalendarDateTime? parseDateTime(
+      String dateTime, CalendarType calendarType) {
     final splitter = dateTime.split(" ");
     final datePart = splitter[0].split("-");
     final timePart = splitter[1].split(":");
 
     try {
       return CalendarDateTime(
-        year: int.parse(datePart[0]),
-        month: int.parse(datePart[1]),
-        day: int.parse(datePart[2]),
-        hour: int.parse(timePart[0]),
-        minute: int.parse(timePart[1]),
-        second: timePart.length == 3 ? double.parse(timePart[2]).toInt() : 0,
-        calendarType: calendarType
-      );
+          year: int.parse(datePart[0]),
+          month: int.parse(datePart[1]),
+          day: int.parse(datePart[2]),
+          hour: int.parse(timePart[0]),
+          minute: int.parse(timePart[1]),
+          second: timePart.length == 3 ? double.parse(timePart[2]).toInt() : 0,
+          calendarType: calendarType);
     } on Exception catch (e) {
       print("${e.toString()}");
       return null;
@@ -50,20 +56,28 @@ class CalendarDateTime {
   }
 
   //supported format 1400-9-12
-  static CalendarDateTime? parseDate(String date,CalendarType calendarType) {
+  static CalendarDateTime? parseDate(String date, CalendarType calendarType) {
     final datePart = date.split("-");
 
     try {
       return CalendarDateTime(
-        year: int.parse(datePart[0]),
-        month: int.parse(datePart[1]),
-        day: int.parse(datePart[2]),
-        calendarType: calendarType
-      );
+          year: int.parse(datePart[0]),
+          month: int.parse(datePart[1]),
+          day: int.parse(datePart[2]),
+          calendarType: calendarType);
     } on Exception catch (e) {
       print("${e.toString()}");
       return null;
     }
+  }
+
+  static CalendarDateTime? parseDateTimeToCalendarTime(DateTime date,CalendarType calendarType) {
+    return CalendarDateTime(
+        year: date.year,
+        month: date.month,
+        day: date.day,
+        calendarType: calendarType);
+
   }
 
   bool isDateEqual(CalendarDateTime dateTime) {
@@ -104,4 +118,83 @@ class CalendarDateTime {
   DateTime toDateTime() {
     return DateTime(year, month, day);
   }
+
+  static List<CalendarDateTime> getDayListCurrentMonth(
+      CalendarDateTime startDate,
+      CalendarDateTime endDate,
+      ) {
+    if (startDate.calendarType == CalendarType.JALALI) {
+      return getDayListJalaliDays(
+          startDate, endDate);
+    } else {
+      return getDayListGregorianDays(startDate,endDate);
+    }
+  }
+
+
+  static List<CalendarDateTime> getDayListJalaliDays(CalendarDateTime startDate,
+      CalendarDateTime endDate) {
+    List<CalendarDateTime> days = [];
+    int counter = 0;
+    int day = startDate.day;
+    int year = startDate.year;
+    int month = startDate.month;
+    int currentMonthDays = monthLength(month, year);
+    int difference= Jalali(startDate.year, startDate.month, startDate.day).distanceTo(Jalali(endDate.year, endDate.month, endDate.day));
+    for (int index = day; counter <= difference; index++) {
+      counter++;
+      if(index > currentMonthDays){
+        month=month+1;
+        if(month==13){
+          month=1;
+          year=year+1;
+          index=1;
+          currentMonthDays = monthLength(month,year);
+        }else if(month<=12 ){
+          index=1;
+          currentMonthDays = monthLength(month,year);
+        }
+
+      }
+
+      days.add(CalendarDateTime(
+          year: year,
+          month: month,
+          day:index,
+          calendarType: CalendarType.JALALI));
+
+
+    }
+    return days;
+
+
+  }
+
+  static List<CalendarDateTime> getDayListGregorianDays(
+      CalendarDateTime startDate,
+      CalendarDateTime endDate,) {
+    List<CalendarDateTime> days = [];
+
+    for (int i = 0; i <= endDate.toDateTime().difference(startDate.toDateTime()).inDays; i++) {
+      days.add( CalendarDateTime.parseDateTime(startDate.toDateTime().add(Duration(days: i)).toString(), CalendarUtils.getCalendarType())! );
+    }
+
+    return days;
+  }
+
+
+  static int  monthLength(int month,int year ) {
+    if (month <= 6) {
+      return 31;
+    } else if (month <= 11) {
+      return 30;
+    } else {
+      // month == 12
+      return Jalali(year).isLeapYear() ? 30 : 29;
+    }
+  }
 }
+
+
+
+

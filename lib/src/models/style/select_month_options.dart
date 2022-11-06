7744ea036b6883c95f01a6dc0e49293eb61f31f4
Amir Jabbari <am.jabbarii@gmail.com>
2022-11-06 09:01:37 +0330

    feat: add onViewTypeChanged

--- lib/src/models/style/select_month_options.dart
+++ lib/src/models/style/select_month_options.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 class MonthOptions{
   String font;
   Color? selectedColor;
+  Color? backgroundColor;
 
-  MonthOptions({this.font = '', this.selectedColor});
+  MonthOptions({this.font = '', this.selectedColor,this.backgroundColor});
 }
